0b0c6234b1e2f00b37beb5266b36e86233231ee5
karimEd001 <anonKM1999@mail.ru>
2024-08-17 00:30:50 +0300

    start, add header and promo slider

---
+++ scripts/slider.js
@@ -0,0 +1,27 @@
+
+// const promoSlider = document.querySelector('.promo-slider');
+
+
+// const swiper1 = new Swiper(promoSlider, {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+
+//     // If we need pagination
+//     // pagination: {
+//     //     el: '.swiper-pagination',
+//     // },
+
+//     // Navigation arrows
+//     // navigation: {
+//     //     nextEl: '.promo-slider__next',
+//     //     prevEl: '.promo-slider__prev',
+//     // },
+
+//     // And if we need scrollbar
+//     // scrollbar: {
+//     //     el: '.swiper-scrollbar',
+//     // },
+// });
+
+// export {swiper1}
