5396934883673588918505f243f0a555d567a903
karimEd001 <anonKM1999@mail.ru>
2024-08-18 23:50:43 +0300

    fix scripts open/close menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,71 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     type: "fraction",
   },
   
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-
 });
 
 const swiperProducts = new Swiper('.swiper.products-slider', {
-  // Optional parameters
   loop: true,
-  // slidesPerView: 1,
-  // spaceBetween: 64,
-
-  // If we need pagination
+  
   pagination: {
     el: '.swiper-pagination.products-slider__pagination',
     type: "fraction",
   },
-  
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-
 });
+
+const burgerMenu = document.querySelector('.burger-menu');
+const hideMenu = document.querySelector('.header-main__hidden');
+
+window.addEventListener('click', (e)=>{
+  checkClickMenu(e);
+})
+burgerMenu.addEventListener('click', ()=>{
+  switchBurger()
+  switchMenu()
+})
+
+function checkClickMenu(e){
+  const targetEl = e.target;
+
+  if(targetEl){
+    if(targetEl.closest('.burger-menu')){
+      return
+    }
+    const menuItemLink = targetEl.closest('a');
+    if(menuItemLink){
+      if(menuItemLink.className.includes('link')){
+        switchMenu()
+        removeActiveClassBurger()
+      }
+    }
+    if(!targetEl.closest('.hidden-menu') && burgerMenu.classList.contains('burger-menu--active')){
+      switchMenu()
+      removeActiveClassBurger()
+    }
+  }
+}
+
+function switchBurger(){
+  burgerMenu.classList.toggle('burger-menu--active')
+}
+
+function removeActiveClassBurger(){
+  burgerMenu.classList.remove('burger-menu--active')
+}
+
+function switchMenu(){
+  hideMenu.classList.toggle('header-main__hidden--active')
+}
