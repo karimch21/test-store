1eb971534efddb3389e58a9055fdd2e7d5828ee0
karimEd001 <anonKM1999@mail.ru>
2024-08-19 00:47:33 +0300

    add prefix, add scripts for 3 dots in product-titile

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination',
     type: "fraction",
   },
-  
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -14,7 +14,7 @@ const swiper = new Swiper('.swiper', {
 
 const swiperProducts = new Swiper('.swiper.products-slider', {
   loop: true,
-  
+
   pagination: {
     el: '.swiper-pagination.products-slider__pagination',
     type: "fraction",
@@ -29,43 +29,61 @@ const swiperProducts = new Swiper('.swiper.products-slider', {
 const burgerMenu = document.querySelector('.burger-menu');
 const hideMenu = document.querySelector('.header-main__hidden');
 
-window.addEventListener('click', (e)=>{
+window.addEventListener('click', (e) => {
   checkClickMenu(e);
 })
-burgerMenu.addEventListener('click', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
+  checkAmountSymbolInTitle();
+});
+burgerMenu.addEventListener('click', () => {
   switchBurger()
   switchMenu()
 })
 
-function checkClickMenu(e){
+function checkClickMenu(e) {
   const targetEl = e.target;
 
-  if(targetEl){
-    if(targetEl.closest('.burger-menu')){
+  if (targetEl) {
+    if (targetEl.closest('.burger-menu')) {
       return
     }
     const menuItemLink = targetEl.closest('a');
-    if(menuItemLink){
-      if(menuItemLink.className.includes('link')){
+    if (menuItemLink) {
+      if (menuItemLink.className.includes('link') && burgerMenu.classList.contains('burger-menu--active')) {
         switchMenu()
         removeActiveClassBurger()
       }
     }
-    if(!targetEl.closest('.hidden-menu') && burgerMenu.classList.contains('burger-menu--active')){
+    if (!targetEl.closest('.hidden-menu') && burgerMenu.classList.contains('burger-menu--active')) {
       switchMenu()
       removeActiveClassBurger()
     }
   }
 }
 
-function switchBurger(){
+function switchBurger() {
   burgerMenu.classList.toggle('burger-menu--active')
 }
 
-function removeActiveClassBurger(){
+function removeActiveClassBurger() {
   burgerMenu.classList.remove('burger-menu--active')
 }
 
-function switchMenu(){
+function switchMenu() {
   hideMenu.classList.toggle('header-main__hidden--active')
 }
+
+
+function checkAmountSymbolInTitle() {
+  const productsCardsTitle = document.querySelectorAll('.products__card-title');
+  const maxSymbol = 29;
+
+  for (let i = 0; i < productsCardsTitle.length; i++) {
+    const textProductsTitle = productsCardsTitle[i].innerText;
+    const amountSymbol = textProductsTitle.length;
+    if (maxSymbol <= amountSymbol) {
+      productsCardsTitle[i].innerText = textProductsTitle.substring(0, maxSymbol).trim() + '...'
+    }
+  }
+}
+
